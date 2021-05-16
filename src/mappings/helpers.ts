3be9fa685aec84d202cafe3496ae783e5ba4ea22
zhangtao <ztopia666@gmail.com>
2021-05-16 22:45:36 +0800

    dev

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,8 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+// export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x6Dd7F02B6025426E46088F598F45212124478182'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
